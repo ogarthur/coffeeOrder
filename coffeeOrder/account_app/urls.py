ba6c007e33d482f0b8c4150bef5a68042943928d
Arturo <olmos.arthur@gmail.com>
2019-03-02 14:16:03 +0100

    no message

--- coffeeOrder/account_app/urls.py
+++ coffeeOrder/account_app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from account_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 #template tag
 app_name = 'account_app'
 
@@ -18,4 +20,4 @@ urlpatterns = [
 
 
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
