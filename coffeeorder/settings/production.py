41320d6534b56819a9f1802cc50992d20e6ecb8a
Arturo <olmos.arthur@gmail.com>
2019-03-09 21:10:23 +0100

    no message

--- coffeeorder/settings/production.py
+++ coffeeorder/settings/production.py
@@ -5,14 +5,14 @@ import dj_database_url
 
 env = os.environ.copy()
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
-       
+
 DEBUG = False
 DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['ogarthur.com', 'https://www.ogarthur.com/', 'dev-coffeorder.herokuapp.com','coffe-order.herokuapp.com','https://dev-coffeorder.herokuapp.com/']
+ALLOWED_HOSTS = ['*','ogarthur.com', 'https://www.ogarthur.com/', 'dev-coffeorder.herokuapp.com','coffe-order.herokuapp.com','https://dev-coffeorder.herokuapp.com/']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 try:
